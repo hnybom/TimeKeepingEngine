dc8bb39f7d00c30e6177f5e9dc3aaf67e4b3ed61
Henri Nybom <henri.nybom@gmail.com>
2012-11-01 17:29:14 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeKeepingEngine::Application.config.secret_token = '2cdfa647962bf5eaefb2227e2cbfda54392112b56e916d61032bf7fd1a0730ff48bc447b62a7dbe5b2a0a973e883b876b206bfa55fbca0aa776beb5a8a2f3e58'
